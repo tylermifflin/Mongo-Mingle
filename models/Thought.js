bf6cb6792b6a3a71c70723dc12ea07efd61fade5
Tyler Mifflin <you@example.com>
2023-06-26 15:42:18 -0600

    set up createdAt, username, and reaction columns to match acceptance criteria

--- models/Thought.js
+++ models/Thought.js
@@ -8,3 +8,18 @@ const thoughtSchema = new Schema(
     minlength: 1,
     maxlength: 280
     },
+    createdAt: {
+    type: Date,
+    default: Date.now,
+    // Use a getter method to format the timestamp on query
+    get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+    type: String,
+    required: true
+    },
+    // use reactionSchema to validate data for a reply
+    reactions: [reactionSchema]
+    },
+    {
+        
