0f4938d9c31d53c53ce2f388e0b01644338e8e23
Tyler Mifflin <you@example.com>
2023-06-26 15:19:12 -0600

    set up email for User and used a regex to make sure it checks for a valid email

--- models/User.js
+++ models/User.js
@@ -9,4 +9,9 @@ const userSchema = new Schema(
     required: true,
     trim: true
     },
-    
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+    },
