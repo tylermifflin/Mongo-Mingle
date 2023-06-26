973c27b059969878a3bc9d9f703fc6dff86b9baa
Tyler Mifflin <you@example.com>
2023-06-26 16:31:29 -0600

    updated getoneuser to populate thoughts and used .select also if no user display error message, set up createUser path

--- userController.js
+++ userController.js
@@ -13,10 +13,28 @@ module.exports = {
     // get one user by id
     async getOneUser(req, res) {
         try {
-            const user = await User.findOne({ _id: req.params.id });
+            const user = await User.findOne({ _id: req.params.id })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v');
+            if (!user) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
             res.json(user);
         }
         catch (err) {
+            res.status(400).json(err);
+        }
+    },
+    // create a new user
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
             res.status(400).json(err);
         }
     },
