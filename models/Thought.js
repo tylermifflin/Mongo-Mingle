a4be419b9c94c31c9630a4243e05bb78512b22ca
Tyler Mifflin <you@example.com>
2023-06-26 15:45:51 -0600

    used to json with virtuals and getters and created virtual for reactionCount, created model and exported it

--- models/Thought.js
+++ models/Thought.js
@@ -21,5 +21,24 @@ const thoughtSchema = new Schema(
     // use reactionSchema to validate data for a reply
     reactions: [reactionSchema]
     },
-    {
-        
+    { 
+    toJSON: {
+    virtuals: true,
+    getters: true
+    },
+    id: false
+    }
+    );
+
+    // get total count of reactions on retrieval
+    thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+    }
+    );
+
+    // create the Thought model using the thoughtSchema
+    const Thought = model('Thought', thoughtSchema);
+
+    // export the Thought model
+    module.exports = Thought;
+
