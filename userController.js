eb59e2c3633245c563ed684f8873eadfaaac0db8
Tyler Mifflin <you@example.com>
2023-06-26 16:43:49 -0600

    set up the create user, updateuser, and delete user, also to remove the user's thoughts when user is deleted

--- userController.js
+++ userController.js
@@ -38,3 +38,33 @@ module.exports = {
             res.status(400).json(err);
         }
     },
+    // update user by id
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate({_id: req.params.id}, req.body, { new: true, runValidators: true });
+            if (!user) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    },
+    // delete user
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.id });
+            if (!user) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            // remove user's associated thoughts from User.thoughts
+            await Thought.deleteMany({ username: user.username });
+            res.json({ message: 'User and associated thoughts deleted!' });
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    },
+    
+
