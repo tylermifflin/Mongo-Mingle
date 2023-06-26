4bd6c6203491b89a71c295375db619d7a9685642
Tyler Mifflin <you@example.com>
2023-06-26 16:58:59 -0600

    set up create thought route, using username and push to connect the thought with the correct user

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -26,3 +26,22 @@ module.exports = {
             res.status(400).json(err);
         }
     },
+    // create a new thought, push the created thought's _id to the associated user's thoughts array field
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            const user = await User.findOneAndUpdate(
+                { username: req.body.username },
+                { $push: { thoughts: thought._id } },
+                { new: true }
+            );
+            if (!user) {
+                res.status(404).json({ message: 'No user found with this username!' });
+                return;
+            }
+            res.json(thought);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    },
+    
