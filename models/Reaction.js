c1a2d0cd3e249c1d2b90839f2c372aa7298c953a
Tyler Mifflin <you@example.com>
2023-06-26 15:55:22 -0600

    set up reactionBody, username, createdAt columns to match acceptance criteria

--- models/Reaction.js
+++ models/Reaction.js
@@ -7,4 +7,19 @@ const reactionSchema = new Schema(
     type: Schema.Types.ObjectId,
     default: () => new Types.ObjectId()
     },
-    
+    reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+    },
+    username: {
+    type: String,
+    required: true
+    },
+    createdAt: {
+    type: Date,
+    default: Date.now,
+    // Use a getter method to format the timestamp on query
+    get: createdAtVal => dateFormat(createdAtVal)
+    }
+    },
