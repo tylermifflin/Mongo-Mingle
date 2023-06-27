d8c45c433aca4588f03a5c967c20fbf517657191
Tyler Mifflin <you@example.com>
2023-06-27 17:16:32 -0600

    realized i needed one more index.js in models to export User and Thought models, was getting error when trying to run it

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('./config/connection');
+const db = require('./config/connection.js');
 const routes = require('./routes');
 
 const PORT = 3001;
