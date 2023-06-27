6eb1d303b6ef4ed7e62302201e872c83f1cb3b19
Tyler Mifflin <you@example.com>
2023-06-27 16:47:03 -0600

    set up the routes using the functions imported to match the setup for thoughController

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,3 +8,17 @@ const {
     createReaction,
     deleteReaction
 } = require('../../controllers/thoughtController.js');
+
+// /api/thoughts
+router.route('/').get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:id
+router.route('/:id').get(getOneThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(createReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
